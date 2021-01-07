a97fa6a9e67e22a1b31498e726e6123eadd69cf4
FRAJ Saber <frejsaber@gmail.com>
2021-01-07 10:03:46 +0100

    import StoreModule in primary appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { StoreModule } from '@ngrx/store';
+import { addProductReducer } from './reducers/product.reducer';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { ProductComponent } from './product/product.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({product:addProductReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]

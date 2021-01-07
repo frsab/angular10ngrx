4dca5e70d55e3631b785ded266cebffbccde8c22
FRAJ Saber <frejsaber@gmail.com>
2021-01-07 10:12:59 +0100

    subscribe to the store

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../app.state';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
+  products: Observable<Product[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<AppState>) {
+    this.products = this.store.select((state) => state.products);
   }
-
+  addProduct(name, price) {
+    this.store.dispatch({
+      type: 'ADD_PRODUCT',
+      payload: <Product>{
+        name: name,
+        price: price,
+      },
+    });
+  }
+  ngOnInit(): void {}
 }
